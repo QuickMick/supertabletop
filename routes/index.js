25aa65b0588a303c37dd90340545c477bfd9f673
mick <figura.m@web.de>
2017-06-06 14:47:15 +0200

    now using pug instead of jade

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,15 @@ var I18N_GAME = require('./../core/i18n_game.json');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
+  // load the correct language and pass it to the jade
   var language = "de-DE";
-  var i18n = JSON.stringify(I18N_GAME[language] || {});
+  var i18n = I18N_GAME[language] || {};
 
-  res.render('index', { title: 'Super Tabletop', I18N_DATA:i18n });
+  res.render('index',
+      {
+        I18N_DATA: JSON.stringify(i18n),  // json-object is sent to the client
+        I18N_LAYOUT:i18n                  // the object is just jused to generate the template
+      });
 });
 
 module.exports = router;
